0b37d40b80a8e93cc05cf685e984c0fb90248857
franidos <franidos@hotmail.com>
2024-04-02 17:00:35 -0500

    release

---
+++ wwwroot/js/utilities/imgUtils.js
@@ -0,0 +1,36 @@
+﻿function handleImages() {
+    let indexImg = 0;
+
+    $("#input-images").sortable({
+        axis: "x",
+        handle: "img",
+        update: function (event, ui) {
+            // Actualiza los identificadores en función del nuevo orden
+            $("#input-images li").each(function (index) {
+                $(this).attr("id", index + 1);
+            });
+        }
+    });
+
+    $("#file-input").on("change", function () {
+        var files = this.files;
+        for (var i = 0; i < files.length; i++) {
+            var file = files[i];
+            renderImage(file);
+        }
+    });
+
+    $("#input-images").on("click", ".delete-button", function (event) {
+        event.stopPropagation();
+        $(this).parent().parent().remove();
+    });
+
+    function renderImage(file) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            indexImg++;
+            $("#input-images").append('<li id="' + indexImg + '"><div><img src="' + e.target.result + '"><button class="delete-button" data-id="' + file.name + '">X</button></div></li>');
+        };
+        reader.readAsDataURL(file);
+    }
+}
---
+++ wwwroot/js/vistas/Response.js
@@ -0,0 +1,57 @@
+﻿function getQueryParam(param) {
+    location.search.substr(1)
+        .split("&")
+        .some(function (item) { // returns first occurence and stops
+            return item.split("=")[0] == param && (param = item.split("=")[1])
+        })
+    return param
+}
+$(document).ready(function () {
+    //llave publica del comercio
+
+    //Referencia de payco que viene por url
+    var ref_payco = getQueryParam('ref_payco');
+    //Url Rest Metodo get, se pasa la llave y la ref_payco como paremetro
+    var urlapp = "https://secure.epayco.co/validation/v1/reference/" + ref_payco;
+
+    $.get(urlapp, function (response) {
+
+
+        if (response.success) {
+
+            if (response.data.x_cod_response == 1) {
+                //Codigo personalizado
+                //alert("Transaccion Aprobada");
+
+                console.log('transacción aceptada');
+            }
+            //Transaccion Rechazada
+            if (response.data.x_cod_response == 2) {
+                console.log('transacción rechazada');
+            }
+            //Transaccion Pendiente
+            if (response.data.x_cod_response == 3) {
+                console.log('transacción pendiente');
+            }
+            //Transaccion Fallida
+            if (response.data.x_cod_response == 4) {
+                console.log('transacción fallida');
+            }
+
+            $('#fecha').html(response.data.x_transaction_date);
+            $('#respuesta').html(response.data.x_response);
+            $('#referencia').text(response.data.x_id_invoice);
+            $('#motivo').text(response.data.x_response_reason_text);
+            $('#recibo').text(response.data.x_transaction_id);
+            $('#banco').text(response.data.x_bank_name);
+            $('#autorizacion').text(response.data.x_approval_code);
+            $('#total').text(response.data.x_amount + ' ' + response.data.x_currency_code);
+
+
+        } else {
+            // alert("Error consultando la información");
+            $('#fecha').html(new Date().toLocaleString());          
+        }
+    });
+
+});
